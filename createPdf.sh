7a19b5e38b545da161d531f6359ffb0fcf246091
Tobias Wyss <towyss@gmail.com>
2024-06-28 16:49:00 +0200

    narrower design

--- createPdf.sh
+++ createPdf.sh
@@ -1,11 +1,30 @@
 #!/bin/bash
 if [ -z "$1" ]; then
-   name="output.tex"
+  name="output"
 else
-  name="${1}.tex"
+  name="${1}"
 fi
 
-pandoc -o "${name}" -f markdown -t latex --template=template.tex *.md --listings
-sed -i 's/\\includegraphics{\(.*\)}/\\begin{figure}[H]\\centering\\includegraphics[width=\\imagewidth]{\1}\\end{figure}/g' output.tex
-pdflatex  -interaction=nonstopmode "${name}"
-rm ${name}
+currentdir=$(pwd)
+if [ -z "$2" ]; then
+  workdir="${currentdir}"
+else
+  workdir="${2}"
+fi
+
+if [ -z "$3" ]; then
+  file="*.md"
+else
+  file="${3}"
+fi
+
+cd "${workdir}"
+
+pandoc -o "${name}.tex" -f markdown -t latex --template="${currentdir}/template.tex" "${file}" --listings
+sed -i '' 's/\\includegraphics{\(.*\)}/\\begin{figure}[H]\\centering\\includegraphics[width=\\imagewidth]{\1}\\end{figure}/g' "${name}.tex"
+pdflatex  -interaction=nonstopmode "${name}.tex"
+rm "${name}.tex"
+rm "${name}.aux"
+rm "${name}.log"
+
+mv "${name}.pdf" "${currentdir}/${name}.pdf"
